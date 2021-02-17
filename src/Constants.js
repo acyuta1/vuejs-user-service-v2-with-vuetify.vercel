97d4f16e28157ff1bd949806d22d55c2645b56ab
Achyutha Aluru <Achyutha.Aluru@Neudesic.com>
2021-02-17 15:43:01 +0530

    user detail

--- src/Constants.js
+++ src/Constants.js
@@ -1,4 +1,4 @@
-export const USER_SERVICE_URL = "http://52.186.32.62:8000/api";
+export const USER_SERVICE_URL = "http://localhost:8000/api";
 
 export const SESSION_ATTRIBUTE_USER_NAME = "username";
 export const SESSION_ATTRIBUTE_JWT = "jwt";
